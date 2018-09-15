204492e59f6c5d65484f71020ad45203415b135a
webyuan <626913501@qq.com>
2018-09-15 20:44:18 +0800

    json test

--- js/partyone.js
+++ js/partyone.js
@@ -17,8 +17,29 @@ $(document).ready(function(){
 			}
 				
 		},
-		err:function(data){
-			console.log("连接失败",data)
+		err:function(data2){
+			$.ajax({
+				url:"./sqljson/party.json",
+				type:'get',
+				dataType:"json",
+				success:function(data){
+					if(data.length>9){
+						for(i=0;i<9;i++){
+							$(".con-lunbo .gider1 ul").append(`<li><a href="./three/party.html#${data[i].id}" title="${data[i].area} ${data[i].jianjie}">【${data[i].name}】${data[i].name}</a>
+							</li>`)
+						}
+					}else{
+						for(i=0;i<data.length;i++){
+							$(".con-lunbo .gider1 ul").append(`<li><a href="./three/party.html#${data[i].id}" title="${data[i].area}+${data[i].jianjie}">【${data[i].name}】${data[i].name}</a>
+							</li>`)
+						}
+					}
+						
+				},
+				err:function(data){
+					console.log("连接失败",data)
+				},
+			});
 		},
 	});
 })
