5aa1185ed08071b278b7e5e5bfd8468da84e3e8d
webyuan <626913501@qq.com>
2018-09-15 20:13:25 +0800

    delete and new jsonfile

---
+++ sqljson/test.php
@@ -0,0 +1,12 @@
+<?php
+// $myfile = fopen("bbs.json", "w")
+$myfile = fopen("answer.json", "w")
+// $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+// $txt = "Bill Gates\n";
+// fwrite($myfile, $txt);
+// $txt = "Steve Jobs\n";
+// fwrite($myfile, $txt);
+// fclose($myfile);
+
+
+?>
