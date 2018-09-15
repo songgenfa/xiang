0c46f16317ee9c2df82f50b33bdc50783a3bd6e4
webyuan <626913501@qq.com>
2018-09-15 21:43:29 +0800

    7

--- js/carsone.js
+++ js/carsone.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$.ajax({
-		url:"./server-cars/getlist.php",
+		url:"./server-cars/getlist0.php",
 		type:'get',
 		dataType:"json",
 		success:function(data){
@@ -10,7 +10,6 @@ $(document).ready(function(){
 				    }
 			)
 			console.log(data)
-			//console.log(data0)
 			if(data.length>9){
 				for(i=0;i<9;i++){
 					$(".con-lunbo .gider2 ul ").append(`<li title="${data[i].dater}-${data[i].timer}"><div style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;"><a href="./three/cars.html#${data[i].id}">
@@ -31,8 +30,43 @@ $(document).ready(function(){
 			}
 				
 		},
-		err:function(data){
-			console.log("连接失败",data)
+		error:function(data){
+			$.ajax({
+				url:"./sqljson/cars.json",
+				type:'get',
+				dataType:"json",
+				success:function(data){
+					data=data.sort(
+						    function(a,b){
+						    	return (b.id-a.id)
+						    }
+					)
+					console.log(data)
+					//console.log(data0)
+					if(data.length>9){
+						for(i=0;i<9;i++){
+							$(".con-lunbo .gider2 ul ").append(`<li title="${data[i].dater}-${data[i].timer}"><div style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;"><a href="./three/cars.html#${data[i].id}">
+											<span>【${data[i].kind}】</span>
+											<span>${data[i].fromplace}-${data[i].toplace}</span><span>|</span>
+											<span>${data[i].dater}-${data[i].timer}</span><span>|</span><span style="color:gray;font-size:12px;">详情</span>
+								</a></div></li>`)
+						}
+					}else{
+						for(i=0;i<data.length;i++){
+
+							$(".con-lunbo .gider2 ul ").append(`<li title="${data[i].dater}-${data[i].timer}"><div style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;"><a href="./three/cars.html#${data[i].id}">
+											<span>【${data[i].kind}】</span>
+											<span>${data[i].fromplace}-${data[i].toplace}</span><span>|</span>
+											<span>${data[i].dater}-${data[i].timer}</span><span>|</span><span style="color:gray;font-size:12px;">详情</span>
+								</a></div></li>`)
+						}
+					}
+						
+				},
+				error:function(data){
+					console.log("连接失败",data)
+				},
+			})
 		},
 	})
 })
